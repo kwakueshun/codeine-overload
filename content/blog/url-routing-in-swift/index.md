225f9930b11da02110de520b3f2ac0364ea7e3ae
Kwaku Eshun <kwaku.eshun16@gmail.com>
2019-04-01 17:24:25 +0000

    initial setup

---
+++ content/blog/url-routing-in-swift/index.md
@@ -0,0 +1,22 @@
+---
+title: Url Routing in Swift
+date: '2019-03-01'
+description: ''
+spoiler: Composing nice Swifty API endpoint urls.
+---
+
+I have noticed for quite some time now a very common pattern in Swift projects when it comes to storing end point urls. To be fair, I have used this more often than I like to admit.
+
+
+<!-- Oh, and here's a great quote from this Wikipedia on
+[salted duck eggs](http://en.wikipedia.org/wiki/Salted_duck_egg). -->
+
+> A salted duck egg is a Chinese preserved food product made by soaking duck
+> eggs in brine, or packing each egg in damp, salted charcoal. In Asian
+> supermarkets, these eggs are sometimes sold covered in a thick layer of salted
+> charcoal paste. The eggs may also be sold with the salted paste removed,
+> wrapped in plastic, and vacuum packed. From the salt curing process, the
+> salted duck eggs have a briny aroma, a gelatin-like egg white and a
+> firm-textured, round yolk that is bright orange-red in color.
+
+<!-- ![Chinese Salty Egg](./salty_egg.jpg) -->
---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+    render() {
+      return (
+        <footer
+          style={{
+            marginTop: rhythm(2.5),
+            paddingTop: rhythm(1),
+          }}
+        >
+          <div style={{ float: 'right' }}>
+            <a href="https://samueleshun.com/" target="_blank" rel="noopener noreferrer">
+              samueleshun.com
+            </a>
+          </div>
+          {/* © {new Date().getFullYear()} */}
+          <a
+            href="https://mobile.twitter.com/samxeshun"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            twitter
+          </a>{' '}
+          &bull;{' '}
+          <a
+            href="https://github.com/kwakueshun"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>{' '}
+          &bull;{' '}
+          {/* <a
+            href="https://stackoverflow.com/users/458193/dan-abramov"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            stack overflow
+          </a> */}
+        </footer>
+      );
+    }
+  }
+
+  export default Footer;
